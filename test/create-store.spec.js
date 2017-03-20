44e0a67f745fa8ddf9d83f0d8f97275e04a1f043
Henry Li <henry1943@163.com>
2017-03-20 13:24:43 +0800

    Update

--- test/create-store.spec.js
+++ test/create-store.spec.js
@@ -1,5 +1,8 @@
 import { createStore } from '../index.js';
 
+import { addTodo, dispatchInMiddle, throwError, unknownAction } from './helpers/actionCreators';
+import * as reducers from './helpers/reducers';
+
 describe('createStore', () => {
   it('expose the public APIs', () => {
     const store = createStore(() => {});
@@ -12,19 +15,142 @@ describe('createStore', () => {
 
   it('throws if reducer is not a function', () => {
     expect(() => {
-      const store = createStore();
+      createStore();
     }).toThrow();
 
     expect(() => {
-      const store = createStore(null);
+      createStore(null);
     }).toThrow();
 
     expect(() => {
-      const store = createStore("test");
+      createStore("test");
     }).toThrow();
 
     expect(() => {
-      const store = createStore({});
+      createStore({});
     }).toThrow();
   });
+
+  it('passes the initial action and the initial state', () => {
+    const store = createStore(reducers.todos, [
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+  });
+
+  it('applies the reducer to the previous state', () => {
+    const store = createStore(reducers.todos);
+    expect(store.getState()).toEqual([]);
+
+    store.dispatch(unknownAction());
+    expect(store.getState()).toEqual([]);
+
+    store.dispatch(addTodo('Hello'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+
+    store.dispatch(addTodo('World'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }, {
+        id: 2,
+        text: 'World'
+      }
+    ]);
+  });
+
+  it('applies the reducer to the initial state', () => {
+    const store = createStore(reducers.todos, [
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+
+    store.dispatch(unknownAction())
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }
+    ]);
+
+    store.dispatch(addTodo('World'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello'
+      }, {
+        id: 2,
+        text: 'World'
+      }
+    ]);
+  });
+
+  it('supports multiple subscriptions', () => {
+    const store = createStore(reducers.todos);
+    const listenerA = jest.fn();
+    const listenerB = jest.fn();
+
+    let unsubscribeA = store.subscribe(listenerA);
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(1);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(2);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    const unsubscribeB = store.subscribe(listenerB);
+    expect(listenerA.mock.calls.length).toBe(2);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(1);
+
+    unsubscribeA();
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(1);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    unsubscribeB();
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    unsubscribeA = store.subscribe(listenerA);
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(4);
+    expect(listenerB.mock.calls.length).toBe(2);
+  })
 });
