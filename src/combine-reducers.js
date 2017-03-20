7f01f900fd2cff9233d39748403f9d68d8bbfa94
Henry Li <henry1943@163.com>
2017-03-20 15:36:11 +0800

    Update

--- src/combine-reducers.js
+++ src/combine-reducers.js
@@ -1,9 +1,31 @@
+import isPlainObject from 'lodash/isPlainObject';
+
 export default function combineReducer(reducerMap) {
-  const keys = Object.keys(reducerMap);
-  return (state, action) => {
+  const keys = Object.keys(reducerMap).filter((key) => {
+    const reducer = reducerMap[key];
+    if (reducer === undefined) {
+      console.error(`No reducer provided for key "${key}"`);
+      return false;
+    } else if (typeof reducer !== 'function') {
+      return false;
+    }
+    return true;
+  });
+  return (state = {}, action) => {
+    if (!isPlainObject(action)) {
+      throw new Error('Action must be a plain object.');
+    }
+    if (action.type === undefined) {
+      throw new Error('Actions may not have an undefined "type" property');
+    }
     const newState = {};
     keys.forEach((key) => {
-      newState[key] = reducerMap[key](state[key], action);
+      const reducer = reducerMap[key];
+      const result = reducer(state[key], action);
+      if (result === undefined) {
+        throw new Error(`The result of action "${action.type}" from reducer "${key}" is undefined.`);
+      }
+      newState[key] = result;
     });
     return newState;
   };
